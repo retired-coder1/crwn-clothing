d649954a483bebcb2dda66cc20e68f8af7a41cce
James <andre152001@MacBook-Pro.attlocal.net>
2020-06-22 20:28:06 -0500

    updating our app with a shop page, collection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import { Switch, Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,22 +1,14 @@
 import React, { Fragment } from 'react'
-// import Directory from '../../components/directory/directory.component'
+import Directory from '../../components/directory/directory.component'
 import './homepage.styles.scss'
-import { Route } from 'react-router-dom'
 
 
-const HatsPage = () => (
-	<div>
-		<h1>Hat's Page</h1>
-	</div>
-)
 
 const HomePage = () => (
 	<Fragment>
-		{/* <div>
+		<div>
 			<Directory />
-		</div> */}
-		<Route exact path='/' component={HomePage}></Route>
-		<Route path='hats' component={HatsPage}></Route>
+		</div>
 	</Fragment>
 )
 
